cf08033eb516dba8d2949495f82b5a6a68cc7672
Charlotte Currey <charlottecurrey7@gmail.com>
2023-05-10 12:19:26 -0400

    finished

--- src/index.js
+++ src/index.js
@@ -1,39 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './style.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./style.css";
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 
 const Red = (props) => {
-    return (
-        <div className='red'>
-            <h1>RED</h1>
-        </div>
-    )
-}
+  return (
+    <div className="red">
+      <h1>RED</h1>
+    </div>
+  );
+};
 
 const Blue = (props) => {
-    return (
-        <div className='blue'>
-            <h1>BLUE</h1>
-        </div>
-    )
-}
+  return (
+    <div className="blue">
+      <h1>BLUE</h1>
+    </div>
+  );
+};
 
 const Main = (props) => {
-    return (
-        <div id='container'>
-
-            <div id='navbar'>
-                {/* navigation here */}
-            </div>
-
-            <div id='main-section'>
-                {/* routes here */}
-            </div>
+  return (
+    <BrowserRouter>
+      <div id="container">
+        <div id="navbar">
+          <Link to="/blue">Go to Blue</Link>
+          <Link to="/red">Go to Red</Link>
         </div>
-    )
-}
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+        <div id="main-section">
+          <Routes>
+            <Route path="/blue" element={<Blue />} />
+            <Route path="/red" element={<Red />} />
+            <Route path="/" element={<Red />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
